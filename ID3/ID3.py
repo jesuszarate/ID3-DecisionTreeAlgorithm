dbae311c6a1d47218286345b47b8249aa367783d
Jesus <jesus.zarate.eng@gmail.com>
2018-02-05 15:01:13 -0700

    Information gain working with any col now

--- ID3/ID3.py
+++ ID3/ID3.py
@@ -25,18 +25,18 @@ class ID3:
         sm = float(len(y))
         return p / sm, n / sm
 
-    def IG(self, df):
+    def IG(self, col, predictand, df):
 
-        feature_set_x1 = df['x1'].drop_duplicates().values
+        feature_set_x1 = df[col].drop_duplicates().values
 
-        arr = []
+        arr = dict()
         for f in feature_set_x1:
-            x = df[df['x1'] == f]['y'].values
+            x = df[df[col] == f][predictand].values
             pos, neg = (id3.proportions(x))
-            arr.append(id3.H(pos, neg))
+            arr[f] = id3.H(pos, neg)
 
-        full_0 = len(df[df['y'] == 0]['y'])/float(len(df))
-        full_1 = len(df[df['y'] == 1]['y'])/float(len(df))
+        full_0 = len(df[df[predictand] == 0][predictand]) / float(len(df))
+        full_1 = len(df[df[predictand] == 1][predictand]) / float(len(df))
 
         # y_vals = df['y'].drop_duplicates().values
         # for i in y_vals:
@@ -48,13 +48,12 @@ class ID3:
 
         sm = 0
         for i in range(len(arr)):
-            s_v = float(len(df[df['x1'] == i])) # Todo: this will only work for x1 right now as is
-            sm += (s_v/float(len(df))) * arr[i]
+            s_v = float(len(df[df[col] == i]))  # Todo: this will only work for x1 right now as is
+            sm += (s_v / float(len(df))) * arr[i]
 
         print ('Sum: {0}'.format(sm))
 
         print ('IG: {0}'.format(H_fullset - sm))
-
 
 
 if __name__ == '__main__':
@@ -68,17 +67,4 @@ if __name__ == '__main__':
 
     df = pd.DataFrame(data=d)
 
-
-
-    # x1_0 = df[df['x1'] == 0]['y'].values
-    # pos0, neg0 = (id3.proportions(x1_0))
-    # print(id3.H(pos0,neg0))
-    #
-    # x1_1 = df[df['x1'] == 1]['y'].values
-    # pos1, neg1 = (id3.proportions(x1_1))
-    # print(id3.H(pos1,neg1))
-    # print
-    # arr = [id3.H(pos0, neg0), id3.H(pos1, neg1)]
-
-    id3.IG(df)
-
+    id3.IG('x3', 'y', df)
